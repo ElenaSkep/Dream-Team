afab307d0e1de0291dd4a3314c857c4815134ac8
ElenaSkep <115921698+ElenaSkep@users.noreply.github.com>
2022-11-16 14:29:50 +0200

    Update User.java
    Validation

--- User.java
+++ User.java
@@ -1,7 +1,14 @@
+import javax.validation.constraints.NotBlank;
+import javax.validation.constraints.Size;
+
 public class User{
+	@NotBlank(message = "Username may not be blank")
 	private String username;
+	@NotBlank(message = "Name may not be blank")
 	private String name;
-	private int password;
+	@Size(min = 8, max = 64, message = "Password must be 8-64 char long")
+	private String password;
+	@NotBlank(message = "Postal code may not be blank")
     private int tk;
     public void setUsername(String username) {
 	this.username = username;
