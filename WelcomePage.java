c7d6de15995ae4b6dc6f8e71f9f6ff13f466f19e
EvelyTheo <115402060+EvelyTheo@users.noreply.github.com>
2022-12-30 14:14:55 +0200

    Update WelcomePage.java

--- WelcomePage.java
+++ WelcomePage.java
@@ -17,35 +17,29 @@ import javax.swing.ImageIcon;
 public class WelcomePage extends JFrame implements ActionListener
 {
 JButton b1;
-JLabel l1;
 JFrame frame = new JFrame();//creates the frame of my application
 	public WelcomePage()
 	{
-	//JFrame frame = new JFrame();//creates the frame of my application//
 	setTitle("Welcome to Door2Door");
 	setSize(420,420);
 	setLocationRelativeTo(null);
 	setDefaultCloseOperation(EXIT_ON_CLOSE);
 	setVisible(true);
-    frame.getContentPane().setBackground(new Color(0,102,0)); //change color of background
+        frame.getContentPane().setBackground(new Color(204,204,204)); //change color of background
 	setLayout(new BorderLayout());
 	setContentPane(new JLabel(new ImageIcon("door2doorlogo.jpg")));
 	setLayout(new FlowLayout());
-	//l1=new JLabel("Join now");//
 	b1=new JButton("Search bar");
-	//add(l1);//
+	b1.addActionListener(this);
 	add(b1);
-	setSize(399,399);
-	setSize(400,400);
-	ImageIcon logoImage = new ImageIcon("C:\\Users\\evely\\Documents\\door2doorlogo.jpg");
+	ImageIcon logoImage = new ImageIcon("door2doorlogo.jpg");
 	frame.setIconImage(logoImage.getImage());
-	//frame.getContentPane().setBackground(new Color(0,102,0)); //change color of background//
+	setVisible(true);
 
 	}
 
     public void actionPerformed(ActionEvent e) {
 			 if(e.getSource() == b1){
-			 l1.setText("Welcome to Door2Door");
 			 System.out.println(e);  
 			 SearchBar k = new SearchBar();
 				}
