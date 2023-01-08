9e749e2c772b50e07be17c2bccd5ce85e68e45c7
Sintivrousai <t8210017@aueb.gr>
2023-01-08 17:05:02 +0200

    latest version of serarated SignUp

---
+++ SignUp.java
@@ -0,0 +1,122 @@
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+import java.sql.Statement;
+import javax.swing.JButton;
+import javax.swing.JFrame;
+import javax.swing.JLabel;
+import javax.swing.JPasswordField;
+import javax.swing.JTextField;
+
+public class SignUp {
+
+	JFrame frame = new JFrame();
+	JLabel userIDLabel;
+	JLabel userPasswordLabel;
+	JLabel areacodeLabel;
+	JTextField userIDField;
+	JTextField areacodeField;
+	JPasswordField userPasswordField;
+	JButton SignUpButton;
+	JButton ResetButton;
+	JLabel messageLabel;
+	int a;
+	Connection con = null;//SQL VARIABLES
+	PreparedStatement pst3 = null;//SQL VARIABLES
+	ResultSet rs3 = null;//SQL VARIABLES
+    public String name;
+    boolean flag;
+	SignUpGui gui;
+	public String myuser;
+	public String areacode;
+
+	 public SignUp(SignUpGui parentGui,JTextField userID,JPasswordField code, JLabel message, JTextField areacode){
+		gui = parentGui;
+		userIDField = userID;
+		userPasswordField = code;
+		messageLabel = message;
+		areacodeField = areacode;
+	}
+
+	 public void signUp1() {
+		 try{
+		 String sql = "INSERT INTO users VALUES (?,?,?);";
+
+
+
+			Class.forName("org.sqlite.JDBC");
+
+
+
+
+			Connection con = DriverManager.getConnection("jdbc:sqlite:src/main/resources/mydb.db");
+
+			//con=Sqlite2.connectiontrial2();
+
+			Statement statement = con.createStatement();
+
+			pst3 = con.prepareStatement(sql); //remember: [PreparedStatement pst3 = null;] at first lines
+
+			//boolean flag=false;
+
+
+
+			//while (flag==false) {
+
+
+				if (userIDField.getText().isEmpty()==true||userIDField.getText()=="testing" ||String. valueOf(userPasswordField.getPassword()).isEmpty()== true ) {
+
+
+					 new SignUp(gui ,userIDField ,userPasswordField ,messageLabel,areacodeField);
+
+					 messageLabel.setText("Please enter username & password");
+					userIDField.getText();
+					userPasswordField.getPassword();
+					//SignUpButton.setText(userIDField.getText());
+					//SignUpButton.setText(String.valueOf(userPasswordField.getPassword()));
+
+
+
+				}else {
+					myuser=userIDField.getText();
+					areacode=areacodeField.getText();
+					flag = true;
+					pst3.setString(1,userIDField.getText());
+					pst3.setString(2,String.valueOf(userPasswordField.getPassword()));
+					pst3.setString(3,areacodeField.getText());
+
+					statement.executeUpdate(sql);
+					pst3.execute();
+					a=1;
+					messageLabel.setText("Sign up successful");
+					new WelcomePage(myuser,areacode);
+
+				}
+
+		}catch (Exception e2){
+			System.out.println("registration failed" + e2);
+		}finally {
+
+			try {
+				if(con != null)
+					con.close();
+				  pst3.close();
+
+			}catch ( SQLException e12) {
+				e12.printStackTrace();
+
+			}
+		}
+}
+
+	 public void reset() {
+		System.out.println("works");
+		userIDField.setText("");
+		userPasswordField.setText("");
+		areacodeField.setText("");
+		messageLabel.setText("Reset successful");
+	 }
+}
+
---
+++ SignUpGui.java
@@ -0,0 +1,91 @@
+import java.awt.Color;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+import javax.swing.JButton;
+import javax.swing.JFrame;
+import javax.swing.JLabel;
+import javax.swing.JPasswordField;
+import javax.swing.JTextField;
+
+public class SignUpGui extends JFrame{
+
+	SignUp signup;
+	JFrame frame = new JFrame();
+	String userID;
+	String code;
+	JLabel userIDLabel;
+	JLabel areacodeLabel;
+	JLabel userPasswordLabel;
+	JTextField userIDField;
+	JTextField areacodeField;
+	JPasswordField userPasswordField;
+	JLabel messageLabel;
+	JButton SignUpButton;
+	JButton ResetButton;
+	
+	private static final long serialVersionUID = 1L;
+
+	SignUpGui(){
+		JFrame frame = new JFrame();//creates the frame of my application
+		frame.setTitle("Sing up to Door2Door"); //sets title of my frame
+		
+	
+		userIDLabel = new JLabel ("Username:");
+		userIDLabel.setBounds(50,100,75,25);//x,y,w,height
+		frame.add(userIDLabel);
+
+		userPasswordLabel = new JLabel ("Password:");
+		userPasswordLabel.setBounds(50,150,75,25);
+		frame.add(userPasswordLabel);
+		
+		messageLabel = new JLabel("The text about the access will appear here"); //message that show if login was successful
+		messageLabel.setBounds(115,240,240,120);
+		frame.add(messageLabel);
+		
+		areacodeField = new JTextField();
+		areacodeField.setBounds(125,200,200,25);
+		frame.add(areacodeField); 
+		 
+		areacodeLabel = new JLabel ("Postal Code:");
+		areacodeLabel.setBounds(50,200,75,25);
+		frame.add(areacodeLabel);
+		 
+		userIDField = new JTextField();
+   		userIDField.setBounds(125,100,200,25);
+   		frame.add(userIDField);
+
+   		userPasswordField = new JPasswordField();
+   		userPasswordField.setBounds(125,150,200,25);
+   		frame.add(userPasswordField);
+
+   		frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);//exit from application
+   		frame.setSize(420,420);//sets the x-dimension, and y-dimension of application
+   		frame.setLayout(null);
+   		frame.setVisible(true);
+   		frame.getContentPane().setBackground(new Color(204,204,204)); //change color of background
+ 
+   		signup = new SignUp(this,userIDField, userPasswordField,messageLabel,areacodeField);
+   		
+		JButton SignUpButton = new JButton("Sign Up");
+		SignUpButton.setBounds(125,240,100,25);
+		SignUpButton.setFocusable(false);
+		SignUpButton.addActionListener(new ActionListener() {
+			public void actionPerformed(ActionEvent e){
+				signup.signUp1();
+				}
+			});
+   		frame.add(SignUpButton);
+   		
+   		JButton ResetButton = new JButton("Reset");
+   		ResetButton.setBounds(225,240,100,25);
+   		ResetButton.setFocusable(false);
+   		ResetButton.addActionListener(new ActionListener() {
+			public  void actionPerformed(ActionEvent e){
+				signup.reset();
+				 System.out.println("works");
+					}
+				});
+   		frame.add(ResetButton);
+	}
+	
+}
