0befb7d368aa30915a1ca4cd433e07320b486e75
IoannaKriatsioti <116463950+IoannaKriatsioti@users.noreply.github.com>
2023-01-11 02:59:26 +0200

    Update GroupChatGraphics.java

--- GroupChatGraphics.java
+++ GroupChatGraphics.java
@@ -9,6 +9,7 @@ public class GroupChatGraphics extends JFrame {
 	JLabel sendMessage;
 	JTextField messageField;
 	JButton messageButton;
+	JButton b1;
 	JLabel messageLabel;
 	String myuser;
 	String areacode;
@@ -35,7 +36,7 @@ public class GroupChatGraphics extends JFrame {
 		frame.setSize(420,420);//sets the x-dimension, and y-dimension of application
 		frame.setLayout(null);
 		frame.setVisible(true);
-		frame.getContentPane().setBackground(new Color(0,102,0));
+		frame.getContentPane().setBackground(new Color(204,204,204));
 
 		messageField = new JTextField();
 		messageField.setBounds(125,100,200,25);
@@ -44,7 +45,7 @@ public class GroupChatGraphics extends JFrame {
         //GroupChatMessages gr= new GroupChatMessages(areacode);
 		GroupChat g = new GroupChat(this,messageField,messageLabel,myuser,areacode);
 
-		messageButton = new JButton("Message");
+		messageButton = new JButton("Send");
 		messageButton.setBounds(125,200,100,25);
 		messageButton.setFocusable(false);
 		messageButton.addActionListener(new ActionListener() {
@@ -55,8 +56,23 @@ public class GroupChatGraphics extends JFrame {
 		}); //I wanted a subject of type Actionlistener
 		frame.add(messageButton);
 
-		//ImageIcon logoImage = new ImageIcon("door2doorlogo.jpg");
-		//frame.setIconImage(logoImage.getImage());
+		frame.setLayout(new BorderLayout());
+
+		b1 = new JButton("Back");
+		b1.setBounds(125,200,100,25);
+		b1.setFocusable(false);
+		b1.addActionListener(new ActionListener() {
+			public  void actionPerformed(ActionEvent e){
+				dispose();
+				System.out.println(e);
+				WelcomePagewith3button k = new WelcomePagewith3button(myuser);
+			}
+		} );
+		frame.add(b1, BorderLayout.SOUTH);
+
+
+		ImageIcon logoImage = new ImageIcon("door2doorlogo.jpg");
+		frame.setIconImage(logoImage.getImage());
 
 		}
 
