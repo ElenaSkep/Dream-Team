0b209f27eed6088864fffdcb07d3602b191f4c14
Elena Skeparnia <elenaskeparn@gmail.com>
2023-01-18 03:43:09 +0200

    Last minute changes

--- MainTest.java
+++
@@ -1,9 +0,0 @@
-package Door2Door;
-
-public class MainTest {
-	public static void main(String [] args) {
-		//new GroupchatGraphics1("elena","15561");
-		new ActionGraphics();
-	}
-}
-
--- door2door/MainTest.java
+++
@@ -1,9 +0,0 @@
-package door2door;
-
-public class MainTest {
-	public static void main(String [] args) {
-		//new GroupchatGraphics1("elena","15561");
-		new ActionGraphics();
-	}
-}
-
